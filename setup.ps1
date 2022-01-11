544bcef48e301d6e1e5285f7f476f290e5960818
nmhung <nmhung.dev@gmail.com>
2022-01-11 18:55:33 +0700

    add: all folder to setup terminal like this image below

---
+++ setup.ps1
@@ -0,0 +1,21 @@
+﻿$userName = $env:UserName
+$pathConfig = "D:\Documents\PowerShell\Config"
+function configTheme() {
+        $pathTheme = "D:\Documents\PowerShell\Modules\oh-my-posh"
+        $versions = Get-ChildItem -Path $pathTheme
+        foreach($item in $versions) {
+            Copy-Item "$($pathConfig)\night-owl.omp.json" -Destination "$($item)\themes"         
+        }
+        Write-Host "Config theme in terminal succesfully"
+}
+
+configTheme
+
+function configTerminal() {
+    
+    $pathDestination = "C:\Users\$($userName)\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+    Copy-Item "$($pathConfig)\settings.json" -Destination $($pathDestination)
+    Write-Host "Config setting terminal succesfully"
+}
+
+configTerminal
