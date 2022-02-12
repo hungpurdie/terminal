8635c832fad1452f538f282f7cf66271901bd89f
hungpurdie <nmhung2022@gmail.com>
2022-02-12 14:56:12 -0800

    feat: update path destination oh my posh theme in script

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,8 @@
 ﻿$userName = $env:UserName
 $pathConfig = Get-Location
 function configTheme() {
-        $pathTheme = "D:\Documents\PowerShell\Modules\oh-my-posh"
-        $versions = Get-ChildItem -Path $pathTheme
-        foreach($item in $versions) {
-            Copy-Item "$($pathConfig)\Config\night-owl.omp.json" -Destination "$($item)\themes"
-        }
+        $pathTheme = "C:\Users\$($userName)\.oh-my-posh"
+        Copy-Item "$($pathConfig)\config\night-owl.omp.json" -Destination "$($pathTheme)\themes"
         Write-Host "Config theme in terminal succesfully"
 }
 
@@ -14,7 +11,7 @@ configTheme
 function configTerminal() {
 
     $pathDestination = "C:\Users\$($userName)\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-    Copy-Item "$($pathConfig)\settings.json" -Destination $($pathDestination)
+    Copy-Item "$($pathConfig)\config\settings.json" -Destination $($pathDestination)
     Write-Host "Config setting terminal succesfully"
 }
 
