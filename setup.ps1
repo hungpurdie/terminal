06afe124a99bf0f03a2350a3e9123f3ca35790c8
hungpurdie <nmhung.dev@gmail.com>
2022-06-28 20:28:51 +0700

    update config terminal

--- setup.ps1
+++ setup.ps1
@@ -1,23 +1,15 @@
 ﻿$userName = $env:UserName
 $pathConfig = Get-Location
+
 function configTheme() {
         $pathTheme = "C:\Users\$($userName)\.oh-my-posh"
         Copy-Item "$($pathConfig)\config\night-owl.omp.json" -Destination "$($pathTheme)\themes"
         Write-Host "Config theme in terminal succesfully"
 }
-configTheme
 
 function configTerminal() {
-
     $pathDestination = "C:\Users\$($userName)\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
     Copy-Item "$($pathConfig)\config\settings.json" -Destination $($pathDestination)
     Write-Host "Config setting terminal succesfully"
 }
 configTerminal
-
-function configPowerShell() {
-    $pathProfilePWShell = "D:\Documents\PowerShell"
-    Copy-Item "$($pathConfig)\Microsoft.PowerShell_profile.ps1" -Destination $($pathProfilePWShell)
-    Write-Host "Config Profile PowerShell in terminal sccesfully"
-}
-configPowerShell
